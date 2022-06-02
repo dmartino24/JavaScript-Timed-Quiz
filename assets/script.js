677071b76d08a0b3715efc78ba7eed4eac865d6d
Dustin Martin <d_man_24_3@hotmail.com>
2022-06-02 09:44:17 -0500

    timer setInterval function completed

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
-var mainContent = document.querySelector("#page-content")
+var mainContent = document.querySelector("#page-content");
 //var startQuizEl = document.querySelector("#questions-container");
-var timerLeftEl = document.querySelector("#timer-left-container");
 var startButtonEl = document.querySelector("#start-btn");
 var colsEls = document.querySelectorAll(".cols");
 var colsRMEls = document.querySelectorAll(".cols-rm");
@@ -8,10 +7,13 @@ var colEl1 = document.querySelector("#col-1");
 var colEl2 = document.querySelector("#col-2");
 var colEl3 = document.querySelector("#col-3");
 var colEl4 = document.querySelector("#col-4");
-
+var timerTextEl = document.querySelector("#timer-left-text");
 var currentTime;
+var totalAnswers;
 var startTime = 75;
 var finishedGame = false;
+var tempQuestions;
+var timerID;
 // available questions to answer
 var questions = [
   {
@@ -22,6 +24,8 @@ var questions = [
       { text: "pink", isAnswer: false },
       { text: "green", isAnswer: false },
     ],
+  },
+  {
     questionTitle: "question 2",
     answers: [
       { text: "blue", isAnswer: true },
@@ -29,6 +33,8 @@ var questions = [
       { text: "pink", isAnswer: false },
       { text: "green", isAnswer: false },
     ],
+  },
+  {
     questionTitle: "question 3",
     answers: [
       { text: "blue", isAnswer: true },
@@ -36,7 +42,64 @@ var questions = [
       { text: "pink", isAnswer: false },
       { text: "green", isAnswer: false },
     ],
-    questionTitle: "question 4 fghg fgh fghfg fghfgh fghfh fgh fghf fgh fghf fghf fghfg fghf fghf fghf fgh fgh",
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle: "question 3",
+    answers: [
+      { text: "blue", isAnswer: true },
+      { text: "red", isAnswer: false },
+      { text: "pink", isAnswer: false },
+      { text: "green", isAnswer: false },
+    ],
+  },
+  {
+    questionTitle:
+      "question 4 fghg fgh fghfg fghfgh fghfh fgh fghf fgh fghf fghf fghfg fghf fghf fghf fgh fgh",
     answers: [
       { text: "blue", isAnswer: true },
       { text: "red", isAnswer: false },
@@ -45,67 +108,122 @@ var questions = [
     ],
   },
 ];
-var getQuestions = function () {
-  var pickedQuestions = [];
-  var uniqueNums = [];
-  var randomNum = Math.floor(Math.random() * questions.length)
-  return pickedQuestions;
+var submitGameScore = function () {
+  timerTextEl.textContent = currentTime;
+  clearInterval(timerID);
+  var pointsRewarded = currentTime;
+  var titleEl = document.createElement("h1");
+  colEl1.appendChild(titleEl);
+  titleEl.textContent = "Well Done!";
+
+  var finalScoreEl = document.createElement("p");
+  colEl2.appendChild(finalScoreEl);
+  finalScoreEl.textContent = "Your final score is " + currentTime + ".";
+  var initialsTextEl = document.createElement("p");
+  var initialsInputEl = document.createElement("input");
+  var initialSubmitButtonEl = document.createElement("button");
+  colEl3.appendChild(initialsTextEl);
+  colEl3.appendChild(initialsInputEl);
+  colEl3.appendChild(initialSubmitButtonEl);
+  initialsTextEl.textContent = "Enter initials: ";
+  initialSubmitButtonEl.textContent = "Submit";
+  initialsInputEl.maxLength = 2;
+  initialSubmitButtonEl.className = "btn";
+  colEl1.style["justify-content"] = "left";
+  colEl3.style["justify-content"] = "left";
+  colEl3.style["align-items"] = "center";
 };
-var answeredQuestionHandler = function(isAnswer) {
-  colEl3.classList.remove();
-  colEl3.className = "col-3-border";
-  var answerResponseEl = document.createElement("h4");
-  colEl3.appendChild(answerResponseEl);
-  if(isAnswer === "true"){
-    answerResponseEl.textContent = "Correct!";
+var checkGameProgress = function () {
+  if (totalAnswers >= 5 || currentTime <= 0) {
+    submitGameScore();
+  } else {
+    createQuizQuestion(getRandomQuestion());
   }
-  else {
+};
+var answeredQuestionHandler = function (isAnswer) {
+  removeColsContent();
+  colEl4.classList.remove();
+  colEl4.className = "col-4-border";
+  var answerResponseEl = document.createElement("h4");
+  colEl4.appendChild(answerResponseEl);
+  if (isAnswer === "true") {
+    answerResponseEl.textContent = "Correct!";
+  } else {
     answerResponseEl.textContent = "Wrong!";
+    currentTime -= 10;
   }
   answerResponseEl.className = "answer-response";
-}
+  totalAnswers += 1;
+  checkGameProgress();
+};
 var createQuizQuestion = function (question) {
   var questionTitleEl = document.createElement("h1");
   colEl1.appendChild(questionTitleEl);
   questionTitleEl.textContent = question.questionTitle;
-  questionTitleEl.className = "question-text";
+  questionTitleEl.className = "align-left";
 
   colEl2.style["justify-content"] = "left";
 
   var questionAnswersULEl = document.createElement("ul");
   colEl2.appendChild(questionAnswersULEl);
 
-  for(var i = 0; i < question.answers.length; i++){
+  for (var i = 0; i < question.answers.length; i++) {
     var questionAnswersLIEl = document.createElement("ol");
     questionAnswersULEl.appendChild(questionAnswersLIEl);
 
     var questionAnswersButtonEl = document.createElement("button");
     questionAnswersLIEl.appendChild(questionAnswersButtonEl);
     questionAnswersButtonEl.className = "btn answer-buttons";
-    questionAnswersButtonEl.textContent = i + 1 + "." + " " + question.answers[i].text;
+    questionAnswersButtonEl.textContent =
+      i + 1 + "." + " " + question.answers[i].text;
     questionAnswersButtonEl.value = question.answers[i].isAnswer;
-    questionAnswersButtonEl.addEventListener("click", function(){
+    questionAnswersButtonEl.addEventListener("click", function () {
       answeredQuestionHandler(this.value);
     });
   }
   //col-2.appendChild
 };
-var removeColsContent = function(){
-  for(var i = 0; i < colsEls.length; i++){
+var getRandomQuestion = function () {
+  var randomNum = Math.floor(Math.random() * tempQuestions.length);
+  var nextQuestion = tempQuestions[randomNum];
+  tempQuestions.splice(randomNum, 1);
+  return nextQuestion;
+};
+var removeColsContent = function () {
+  for (var i = 0; i < colsEls.length; i++) {
     var child = colsEls[i].lastElementChild;
-    while(child){
+    while (child) {
       colsEls[i].removeChild(child);
       child = colsEls[i].lastElementChild;
     }
   }
-}
+};
+var removeColsRMContent = function () {
+  for (var i = 0; i < colsRMEls.length; i++) {
+    var child = colsRMEls[i].lastElementChild;
+    while (child) {
+      colsRMEls[i].removeChild(child);
+      child = colsRMEls[i].lastElementChild;
+    }
+  }
+};
+var startTimer = function () {
+  timerID = setInterval(function () {
+    currentTime -= 1;
+    timerTextEl.textContent = currentTime;
+  }, 1000);
+};
 //starts the quiz
 var startGame = function () {
-  var questionsInGame = getQuestions();
-  // loop will go here
+  currentTime = startTime;
+  totalAnswers = 0;
+  finishedGame = false;
+  tempQuestions = [];
+  tempQuestions = tempQuestions.concat(questions);
+
   removeColsContent();
-  createQuizQuestion(questions[0]);
-  
+  createQuizQuestion(getRandomQuestion());
+  startTimer();
 };
 // on mouse click triggers startGame function
 startButtonEl.addEventListener("click", startGame);
